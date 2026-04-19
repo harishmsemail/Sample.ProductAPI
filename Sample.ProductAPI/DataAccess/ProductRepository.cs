9d08edf068b76c54f760f9ef36025493f685874c
harishmsemail <73133336+harishmsemail@users.noreply.github.com>
2026-04-19 17:09:08 -0400

    Update ProductRepository.cs

--- Sample.ProductAPI/DataAccess/ProductRepository.cs
+++ Sample.ProductAPI/DataAccess/ProductRepository.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Sample.ProductAPI.Dtos;
+using Sample.ProductAPI.Mappers;
 using Sample.ProductAPI.Models;
 
 namespace Sample.ProductAPI.DataAccess
@@ -20,24 +22,36 @@ namespace Sample.ProductAPI.DataAccess
         }
 
         /// <inheritdoc />
-        public async Task<Product?> GetProductAsync(int productId)
+        public async Task<ProductDto?> GetProductAsync(int productId)
         {
-            // Retrieve a single product by its ID.
-            return await _context.Products.FindAsync(productId);
+            // Retrieve a single product by its ID, including its attributes.
+            var product = await _context.Products
+                .Include(p => p.Attributes)
+                .FirstOrDefaultAsync(p => p.ProductId == productId);
+
+            return product?.ToDto();
         }
 
         /// <inheritdoc />
-        public async Task<IEnumerable<Product>> GetProductsAsync()
+        public async Task<IEnumerable<ProductDto>> GetProductsAsync()
         {
             // Retrieve all products, ordered by average customer rating in descending order.
-            return await _context.Products.OrderByDescending(p => p.AverageCustomerRating).ToListAsync();
+            var products = await _context.Products
+                .Include(p => p.Attributes)
+                .OrderByDescending(p => p.AverageCustomerRating)
+                .ToListAsync();
+
+            return products.Select(p => p.ToDto());
         }
 
         /// <inheritdoc />
-        public async Task<IEnumerable<ProductAttribute>> GetProductAttributesAsync(int productId)
+        public async Task<IEnumerable<ProductAttributeDto>> GetProductAttributesAsync(int productId)
         {
             // Retrieve all attributes for a given product ID.
-            return await _context.ProductAttributes.Where(pa => pa.ProductId == productId).ToListAsync();
+            return await _context.ProductAttributes
+                .Where(pa => pa.ProductId == productId)
+                .Select(pa => pa.ToDto())
+                .ToListAsync();
         }
     }
 }
